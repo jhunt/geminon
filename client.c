2aea5e8fcca0513afcfe772bfeedcd3e06b53209
James Hunt <james@niftylogic.com>
2021-07-28 00:29:08 +0000

    Make gurl client better
    It now supports CLI flags, environment pre-configuration, GAI
    lookups, and more!

--- client.c
+++ client.c
@@ -6,6 +6,10 @@
 #include <arpa/inet.h>
 #include <netinet/ip.h>
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
 #include <openssl/ssl.h>
 #include <openssl/err.h>
 
@@ -21,14 +25,16 @@ int gemini_client_tls(struct gemini_client *client, const char *cert, const char
 		return -1;
 	}
 
-	if (SSL_CTX_use_certificate_file(client->ssl, cert, SSL_FILETYPE_PEM) <= 0) {
-		ERR_print_errors_fp(stderr);
-		return -2;
-	}
+	if (cert && key) {
+		if (SSL_CTX_use_certificate_file(client->ssl, cert, SSL_FILETYPE_PEM) <= 0) {
+			ERR_print_errors_fp(stderr);
+			return -2;
+		}
 
-	if (SSL_CTX_use_PrivateKey_file(client->ssl, key, SSL_FILETYPE_PEM) <= 0) {
-		ERR_print_errors_fp(stderr);
-		return -3;
+		if (SSL_CTX_use_PrivateKey_file(client->ssl, key, SSL_FILETYPE_PEM) <= 0) {
+			ERR_print_errors_fp(stderr);
+			return -3;
+		}
 	}
 
 	SSL_CTX_set_verify(client->ssl, SSL_VERIFY_NONE, all_ok);
@@ -38,23 +44,56 @@ int gemini_client_tls(struct gemini_client *client, const char *cert, const char
 struct gemini_response * gemini_client_request(struct gemini_client *client, const char *url) {
 	int fd, rc;
 	struct gemini_response *res;
-	struct sockaddr_in sa;
+	struct addrinfo *info, hint, *rp;
+	struct gemini_url *u;
 
-	rc = inet_pton(AF_INET, "127.0.0.1", &sa.sin_addr);
-	if (rc != 1) {
+	int port;
+	char p[6], *service;
+
+	char host[NI_MAXHOST];
+
+	u = gemini_parse_url(url);
+	if (!u) {
 		return NULL;
 	}
-	sa.sin_family      = AF_INET;
-	sa.sin_port        = htons(1964);
 
-	fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (fd < 0) {
+	port = u->port;
+	service = p + 5;
+	*service-- = '\0';
+	while (service >= p && port > 0) {
+		*service-- = '0' + (port % 10);
+		port = port / 10;
+	}
+	service++;
+
+	memset(&hint, 0, sizeof(hint));
+	hint.ai_family = AF_INET;
+	hint.ai_socktype = SOCK_STREAM;
+	fprintf(stderr, "looking up '%s' (port '%s')...\n", u->host, service);
+	rc = getaddrinfo(u->host, service, &hint, &info);
+	if (rc != 0) {
+		free(u);
+		fprintf(stderr, "addrinfo error, lookup fail:  %s\n", gai_strerror(rc));
 		return NULL;
 	}
 
-	rc = connect(fd, (struct sockaddr *)&sa, sizeof(sa));
-	if (rc < 0) {
+	fd = -1;
+	for (rp = info; rp != NULL; rp = rp->ai_next) {
+		fd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+		if (fd < 0) continue;
+
+		getnameinfo(rp->ai_addr, rp->ai_addrlen, host, sizeof(host), NULL, 0, NI_NUMERICHOST);
+		fprintf(stderr, "connecting to %s (via %s)\n", u->host, host);
+
+		rc = connect(fd, rp->ai_addr, rp->ai_addrlen);
+		if (rc == 0) break;
+
 		close(fd);
+		fd = -1;
+	}
+	freeaddrinfo(info);
+
+	if (fd < 0) {
 		return NULL;
 	}
 
@@ -65,9 +104,18 @@ struct gemini_response * gemini_client_request(struct gemini_client *client, con
 	}
 
 	res->ssl = SSL_new(client->ssl);
+	if (!res->ssl) {
+		fprintf(stderr, "ssl setup failed\n");
+		ERR_print_errors_fp(stderr);
+		close(fd);
+		free(res);
+		return NULL;
+	}
 	SSL_set_fd(res->ssl, fd);
 
 	if (SSL_connect(res->ssl) != 1) {
+		fprintf(stderr, "ssl handshake failed\n");
+		ERR_print_errors_fp(stderr);
 		close(fd);
 		SSL_free(res->ssl);
 		free(res);
--- gurl.c
+++ gurl.c
@@ -2,31 +2,91 @@
 #include <errno.h>
 #include <string.h>
 
+#include <getopt.h>
+
 #include "./gemini.h"
 
+int configure(struct gemini_client *client, int argc, char **argv, char **envp) {
+	int rc, c, idx;
+	char *cert = NULL, *key = NULL;
+
+	struct option options[] = {
+		{ "tls-certificate", required_argument, NULL, 'c' },
+		{ "tls-key",         required_argument, NULL, 'k' },
+		{ 0, 0, 0, 0 },
+	};
+
+	/* first, we try the environment */
+	cert = getenv("GEMINI_CLIENT_CERTIFICATE");
+	if (cert) cert = strdup(cert);
+
+	key = getenv("GEMINI_CLIENT_PRIVATE_KEY");
+	if (key) key = strdup(key);
+
+	/* then, we try the command line */
+	while (1) {
+		idx = 0;
+		c = getopt_long(argc, argv, "c:k:", options, &idx);
+		if (c == -1)
+			break;
+
+		switch (c) {
+			case 'c':
+				free(cert);
+				cert = strdup(optarg);
+				break;
+
+			case 'k':
+				free(key);
+				key = strdup(optarg);
+				break;
+		}
+	}
+
+	if ((cert && !key) || (!cert && key)) {
+		fprintf(stderr, "you must specify both a TLS X.509 certificate and private key via the --tls-certificate=/path and --tls-key=/path options.\n");
+		return -1;
+	}
+
+	rc = gemini_client_tls(client, cert, key);
+	if (rc != 0) {
+		fprintf(stderr, "tls configuration failed: %s (error %d)\n", strerror(errno), errno);
+		return -1;
+	}
+	if (cert && key) {
+		printf("loading tls certificate from %s\n", cert);
+		printf("loading tls private key from %s\n", key);
+		free(cert);
+		free(key);
+	}
+
+	return 0;
+}
+
 int main(int argc, char **argv, char **envp) {
-	int rc;
+	int rc, nreqs;
 	struct gemini_client client;
 	struct gemini_response *res;
 
 	memset(&client, 0, sizeof(client));
-	rc = gemini_client_tls(&client, "cert.pem", "key.pem");
+	rc = configure(&client, argc, argv, envp);
 	if (rc != 0) {
-		fprintf(stderr, "gemini_client_tls() failed! (e%d: %s)\n", errno, strerror(errno));
 		return 1;
 	}
 
-	res = gemini_client_request(&client, "gemini://127.0.0.1:1964/echo/hello/hello/there");
-	if (!res) {
-		fprintf(stderr, "gemini_client_request() failed! (e%d: %s)\n", errno, strerror(errno));
-		return 1;
-	}
+	for (nreqs = 0; optind < argc; nreqs++, optind++) {
+		res = gemini_client_request(&client, argv[optind]);
+		if (!res) {
+			fprintf(stderr, "gemini_client_request() failed! (e%d: %s)\n", errno, strerror(errno));
+			return 1;
+		}
 
-	rc = gemini_response_stream(res, 1, 8192);
-	if (rc != 0) {
-		fprintf(stderr, "gemini_response_stream() failed! (e%d: %s)\n", errno, strerror(errno));
-		return 1;
+		rc = gemini_response_stream(res, 1, 8192);
+		if (rc != 0) {
+			fprintf(stderr, "gemini_response_stream() failed! (e%d: %s)\n", errno, strerror(errno));
+			return 1;
+		}
+		gemini_response_close(res);
 	}
-	gemini_response_close(res);
 	return 0;
 }
